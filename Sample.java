fbcbc57aed95340596bd6d0a664226f373396e10
Pranjal Sharma <pranjalsharma0211@gmail.com>
2026-01-11 21:43:48 -0800

    house Robber solution

--- Sample.java
+++ Sample.java
@@ -1,8 +1,30 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
-
+// Time Complexity :O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : 
 
 // Your code here along with comments explaining your approach
-//Hello
+/* 
+At every house, you have 2 choices, either rob it or skip it.
+	1.	Skip the current house → keep previous maximum.
+	2.	Rob the current house → add its money to the best value from two houses back.
+*/ 
+
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        //base 
+        if (n==0) return 0;
+        if (n==1) return nums[0];
+
+        int[] dp = new int[n];
+        int prev=nums[0];
+        int curr= Math.max(nums[0], nums[1]);
+        for (int i = 2; i<n; i++){
+            int temp = curr;
+            curr= Math.max(curr, nums[i] + prev);
+            prev=temp;
+        }
+        return curr;
+    }
+}
